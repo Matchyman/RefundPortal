fd215e5f6de31e0571c9b394eee76d773d0cb05b
Matchyman <jmatcham31@googlemail.com>
2021-06-16 10:47:55 +0100

    missed tcs accepted

--- schema.sql
+++ schema.sql
@@ -5,8 +5,32 @@ CREATE TABLE refunds(
     last_name varChar(50) NOT NULL,
     student_number char(7) NOT NULL,
     
+    payer_title varChar(10),
+    payer_first_name varChar(50),
+    payer_last_name varChar(50),
+    payer_address text,
+
+    acc_name_it varChar(50),
+    acc_iban_it varChar(15),
+    acc_swift_it varChar(15),
+    acc_bank_name_it varChar(30),
+    acc_bank_address_it text,
+
+    acc_name_ht varChar(50),
+    acc_num_ht char(8),
+    acc_sort_code char(9),
+
+    ref_reason text,
+    visa_ref boolean,
+    visa_ref_file image,
+    ref_ex_reason text,
+    tcs_accepted  boolean,
+
     int_accept boolean,
-    int_accept_date date,
+    int_dec_date date,
+    int_rej-reason text,
     fi_accept boolean,
-    fi_accept_date date,
+    fi_dec_date date,
+    fi_rej_reason text,
+
 );
