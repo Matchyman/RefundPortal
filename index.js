882b3943266af01c6a456680dec7f7cf9120ef25
Matchyman <jmatcham31@googlemail.com>
2021-06-23 11:16:26 +0100

    update for accept/deny international team

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { body, validationResult, sanitizeBody, check, oneOf } = require('express-validator');
+const { body, validationResult, sanitizeBody, check, oneOf, query } = require('express-validator');
 const multer = require('multer');
-//File Uploading need to reroute to db
-var upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: 'uploads/' });
 const app = express()
 const port = 3000;
 
 
 // =========== MSSQL INFORMATION ===========
 const sql = require('mssql');
-const { request } = require('express');
+const { request, application } = require('express');
 
 const sqlConfig = {
     user: 'RefundAuth',
@@ -29,6 +28,7 @@ const sqlConfig = {
 }
 
 // ==========================================
+
 
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public/')); // Assets go in the public folder.
@@ -79,7 +79,12 @@ app.post('/application',
     }
     */
 
-    //File Uploading
+    /*
+        File shit
+        Hold in memory
+    */
+
+    //Custom validator for image file
     upload.single('ref-notice-file'),
 
     //@portal-transfer validation
@@ -100,7 +105,9 @@ app.post('/application',
     body('ref-bank-address-it').optional({ checkFalsy: true }).escape().isAlphanumeric().withMessage('Please do not use any symbols'),
 
     //@home-transfer validation
-    body('ref-acc-name-ht').optional({ checkFalsy: true }).escape().isLength({ min: 1 }).withMessage('Empty account name').isAlpha().withMessage('must contain alphabet letters'),
+    check('ref-acc-name-ht').optional({ checkFalsy: true }).custom((value) => {
+        return value.match(/^[A-Za-z ]+$/);
+    }).withMessage("Please only use alphabetic letters").escape(),
     body('ref-acc-num-ht').optional({ checkFalsy: true }).trim().escape().isLength({ min: 8, max: 8 }).withMessage('Account Number must be 8 digits long').isNumeric().withMessage('Must only use numbers'),
     check('ref-sort-code-ht').optional({ checkFalsy: true }).escape().isAlphanumeric().withMessage('Must only contain digits'),
     check('ref-sort-code-ht1').optional({ checkFalsy: true }).escape().isAlphanumeric().withMessage('Must only contain digits'),
@@ -108,29 +115,39 @@ app.post('/application',
 
     //@portal-extra2 validation
     body('ref-reason').optional({ checkFalsy: true }).trim().escape().isAlphanumeric().withMessage('Text may only contain alphanumeric characters'),
+    check('ref-notice-file').optional({ checkFalsy: true }).custom((value, { req }) => {
+        if (req.file.mimetype === 'image/png') {
+            return '.png';
+        } else if (req.file.mimetype === 'image/png') {
+            return '.jpeg';
+        } else {
+            return false;
+        }
+    }).withMessage("Please only upload png or jpeg documents"),
     body('ref-ex-reason').optional({ checkFalsy: true }).trim().escape().isAlphanumeric().withMessage('Text may only contain alphanumeric characters'),
     body('t/c-accepted').toBoolean(),
 
     async(req, res) => {
-
+        console.log(req.file);
         const errors = validationResult(req);
         console.log(req.body);
 
         if (!errors.isEmpty()) {
             //console.log(errors.array());
-
             res.render('application.html', { errors: errors.array() });
         } else {
             console.log("Free of errors insert into database")
+            await sql.connect(sqlConfig)
+            const request = new sql.Request()
+            request.input('student_number', req.body['student-number'])
             try {
-                await sql.connect(sqlConfig)
-                const request = new sql.Request()
+
 
                 request.input('stu_pay', req.body['stu-pay'])
                 request.input('title', req.body['ref-title'])
                 request.input('first_name', req.body['ref-first-name'])
                 request.input('last_name', req.body['ref-last-name'])
-                request.input('student_number', req.body['student-number'])
+
 
                 request.input('payer_title', req.body['ref-payer-title'])
                 request.input('payer_first_name', req.body['ref-payer-first-name'])
@@ -148,26 +165,30 @@ app.post('/application',
                 request.input('sort_code_ht', req.body['ref-sort-code-ht'] + req.body['ref-sort-code-ht1'] + req.body['ref-sort-code-ht2'])
 
                 request.input('reason', req.body['ref-reason'])
+                console.log(req.file['filename']);
+                const fileinput = req.file['path'];
+                console.log(fileinput)
                 request.input('ex_reasons', req.body['ref-ex-reasons'])
 
                 request.query('INSERT INTO refunds' +
-                    '(pay_type, title, first_name, last_name, student_number,' +
-                    'payer_title, payer_first_name, payer_last_name, payer_address,' +
-                    'acc_name_it, acc_iban_it, acc_swift_it, acc_bank_name_it, acc_bank_address_it,' +
-                    'acc_name_ht, acc_num_ht, acc_sort_code, ref_reason, ref_ex_reason) VALUES' +
+                        '(pay_type, title, first_name, last_name, student_number,' +
+                        'payer_title, payer_first_name, payer_last_name, payer_address,' +
+                        'acc_name_it, acc_iban_it, acc_swift_it, acc_bank_name_it, acc_bank_address_it,' +
+                        'acc_name_ht, acc_num_ht, acc_sort_code, ref_reason, ref_ex_reason) VALUES' +
 
-                    '(@stu_pay, @title, @first_name,@last_name, @student_number,' +
-                    '@payer_title, @payer_first_name, @payer_last_name,@payer_address,' +
-                    '@acc_name_it, @acc_num_it, @swift_code_it, @bank_name_it, @bank_address_it,' +
-                    '@acc_name_ht, @acc_num_ht, @sort_code_ht, @reason, @ex_reasons);')
-
-
+                        '(@stu_pay, @title, @first_name,@last_name, @student_number,' +
+                        '@payer_title, @payer_first_name, @payer_last_name,@payer_address,' +
+                        '@acc_name_it, @acc_num_it, @swift_code_it, @bank_name_it, @bank_address_it,' +
+                        '@acc_name_ht, @acc_num_ht, @sort_code_ht, @reason, @ex_reasons);'
+                    )
+                    //@TODO Need to really fix inputing files. Really struggling
+                    //request.query(`UPDATE refunds SET visa_ref_file = ${fileinput} WHERE student_number = @student_number`);
             } catch (error) {
                 console.log(error)
             }
 
             console.log("Application Recieved Send email");
-
+            sendEmail();
             res.redirect('application');
         }
 
@@ -182,14 +203,47 @@ app.post('/postLogin', async(req, res) => { // @TODO: Dependant on login/authent
     await sql.connect(sqlConfig)
     const request = new sql.Request()
     request.query('select * from refunds', (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render('management.html', { data: result });
-            }
-        })
-        // console.log(result[0].userID); // -- Ensuring a valid entry is returned.
-        //res.redirect('login.html');
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('management.html', { data: result });
+        }
+    })
+
+    //res.redirect('login.html');
+
+})
+
+//@TODO Implement post methods for the accept/deny logic
+app.post('/intsubmission', async(req, res) => {
+    console.log(req.body);
+
+    await sql.connect(sqlConfig)
+    const request = new sql.Request()
+
+    //request.input('date', date.now())
+    request.input('stu_num', req.body['stu_num'])
+    if (req.body['intAccept'] === 'true') {
+        console.log('Update Database with Accept')
+        request.input('input_param', sql.Bit, req.body['intAccept'])
+        request.query('update refunds set int_accept = @input_param where student_number = @stu_num ');
+
+        console.log('Update Database with Accept')
+        sendEmail()
+    } else {
+        console.log('Update Database with Deny')
+        request.input('input_param', sql.Bit, req.body['intAccept'])
+        request.query('update refunds set int_accept = @input_param where student_number = @stu_num ');
+        sendEmail()
+    }
+    request.input('input_parameter', "portal")
+    request.query('select * from refunds where pay_type = @input_parameter', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('management.html', { data: result })
+        }
+    })
 
 })
 
@@ -201,3 +255,7 @@ app.post('/search', (req, res) => { //
 app.listen(port, () => {
     console.log(`Application started @ http://localhost:${port}`)
 })
+
+function sendEmail() {
+
+}
